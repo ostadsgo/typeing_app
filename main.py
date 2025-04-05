cbea73482eba0ad25a00bdb630382666e0c321e5
Saeed Gholami <saeed.ghollami@gmail.com>
2025-04-06 01:29:41 +0330

    Type over text using curses.

--- main.py
+++ main.py
@@ -1,26 +1,43 @@
+import os
+import sys
 import keyboard
+import getpass
+import curses
+
 
 class Game:
     def __init__(self, text):
         self.text = text
-        self.sample_text = "the quick brown fox jumps over lazy dog."
+        self.text = "the quick brown fox jumps over lazy dog."
         self.pointer_pos = 0
         self.score = 0
+        self.typed = ""
 
     def play(self):
-        print(self.sample_text)
-        print('^')
+        self.redraw_screen()
         keyboard.on_release(self.on_keypress)
         user_resp = input("")
-        print(f"Your socre is: {self.score}")
+        sys.exit()
+
+    def redraw_screen(self):
+        print(self.typed)
+        os.system("clear")
+        print(self.text)
+        print(self.pointer_pos * ' ' + '^')
+        print(self.typed)
 
     def on_keypress(self, event):
         pressed_key = event.name
-        char_under_pointer = self.sample_text[self.pointer_pos]
-        # print("Pressed Key: ", pressed_key, "Char Under Pointer", char_under_pointer )
+        char_under_pointer = self.text[self.pointer_pos]
+        self.typed += pressed_key
         if pressed_key == char_under_pointer:
             self.score += 1
         self.pointer_pos += 1  # move to next char
+        self.redraw_screen()
+        if len(self.typed) >= len(self.text):
+            print(f"Your socre is: {self.score}")
+            sys.exit()
+            
 
 class App:
     def readfile(self, filename):
